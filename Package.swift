427690e4be2b59e22c5ccce097d2affd17aa75e8
Tomoaki Yagishita <tyagishi@gmail.com>
2023-10-17 11:47:27 +0900

    use lowercased name for command

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
-        .executable(name: "SwiftyMermaid", targets: ["SwiftyMermaid"]),
+        .executable(name: "swiftymermaid", targets: ["swiftymermaid"]),
     ],
     dependencies: [
         .package(url: "https://github.com/sdidla/Hatch", from: "508.0.0"),
@@ -19,13 +19,13 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(name: "SwiftyMermaid",
+        .executableTarget(name: "swiftymermaid",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "HatchParser", package: "Hatch")]
         ),
         .testTarget(
             name: "SwiftyMermaidTests",
-            dependencies: ["SwiftyMermaid"]),
+            dependencies: ["swiftymermaid"]),
     ]
 )
